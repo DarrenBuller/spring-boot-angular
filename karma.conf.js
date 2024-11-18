8e690e5058bb2e083acdb0a44c4ec78b525b7501
Darren Buller <stash-taco-remake@duck.com>
2024-11-18 13:57:27 +0000

    Include coverage checks in ci test

--- karma.conf.js
+++ karma.conf.js
@@ -24,12 +24,20 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/under-construction'),
+      dir: require('path').join(__dirname, './coverage/spring-boot-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-      ]
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
